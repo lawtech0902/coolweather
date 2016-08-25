b7368437ce7df56468e6810ed8f554be49982f33
Lawtech <584563542@qq.com>
2016-08-25 15:31:10 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ app/src/main/java/com/example/lawtech/coolweather/model/City.java
@@ -0,0 +1,43 @@
+package com.example.lawtech.coolweather.model;
+
+/**
+ * Created by lawtech on 16/8/24.
+ */
+public class City {
+    private int id;
+    private String cityName;
+    private String cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
